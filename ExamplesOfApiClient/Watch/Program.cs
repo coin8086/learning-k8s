8c37afe356cb57ee11117b14eaa16e01d62f631b
Robert Zhang <louirobert@gmail.com>
2025-04-17 09:19:23 +0800

    Add ExamplesOfApiClient/Watch

---
+++ ExamplesOfApiClient/Watch/Program.cs
@@ -0,0 +1,23 @@
+﻿using k8s.Models;
+using k8s;
+
+namespace Watch;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        var config = KubernetesClientConfiguration.BuildDefaultConfig();
+        IKubernetes client = new Kubernetes(config);
+
+        var podlistResp = client.CoreV1.ListNamespacedPodWithHttpMessagesAsync("default", watch: true);
+
+        await foreach (var (type, item) in podlistResp.WatchAsync<V1Pod, V1PodList>())
+        {
+            Console.WriteLine("\n==on watch event==");
+            Console.WriteLine(type);
+            Console.WriteLine(item.Metadata.Name);
+            Console.WriteLine(item.Status.Phase);
+        }
+    }
+}
